0ff0962cac812def048b516d0c9290d2e2d34c8f
= <=>
2020-10-22 21:57:55 +0200

    Add project files.

---
+++ PW Clock/ModEntry.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using Harmony;
+using UnityEngine;
+using TMPro;
+
+namespace PW_Clock
+{
+    public class ModEntry : MelonMod
+    {
+        static GameObject gunHud;
+        static GameObject clockGO;
+        static TMP_Text text = null;
+        static DateTime currentTime;
+
+        public override void OnApplicationStart()
+        {
+            // Load configuration and overwrite any custom tracks
+
+
+        }
+
+        [HarmonyPatch(typeof(Gun), "Fire", new System.Type[0] {})]
+        public static class GunMod
+        {
+            static bool fired = false;
+            public static void Postfix(Gun __instance)
+            {
+                if (fired == true && gunHud != null)
+                {
+                    return;
+                }
+
+
+                gunHud = GameObject.Find("/UnityXR_VRCameraRig(Clone)/TrackingSpace/Right Hand/Pointer/").transform.GetChild(0).Find("Pivot").Find("MenuParent").Find("LeftGunMenu").Find("Pivot").gameObject;
+                                       
+                GameObject multiplierDisplay = gunHud.transform.Find("MultiplierDisplay").gameObject;
+                                                                
+                clockGO = GameObject.Instantiate(multiplierDisplay);
+                clockGO.name = "PW Clock";
+                clockGO.transform.parent = gunHud.transform;
+                UnityEngine.Object.Destroy(clockGO.GetComponent<IntDisplay>());
+                UnityEngine.Object.Destroy(clockGO.transform.Find("Value").gameObject);
+
+                text = clockGO.GetComponentInChildren<TMP_Text>();
+                RectTransform rect = text.GetComponent<RectTransform>();
+                rect.sizeDelta = new Vector2(1.2f, 0.5f);
+                text.richText = true;
+                text.fontStyle = FontStyles.Normal;
+                clockGO.transform.localPosition = new Vector3(-0.015f, 0.06f, 0);
+                clockGO.transform.localRotation = new Quaternion(0, 180, 0, 0);
+
+
+
+                //TODO fix transform
+                currentTime = DateTime.Now;
+                fired = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Gun), "OnDisable", new System.Type[0] { })]
+        public static class GunOnDisableMod
+        {
+            public static void Postfix(Gun __instance)
+            {
+                gunHud = null;
+                GameObject.Destroy(clockGO);
+            }
+        }
+
+        
+
+        public override void OnUpdate()
+        {
+            if (text != null)
+            {
+                DateTime time = DateTime.Now;
+                if (currentTime != time)
+                {
+                    currentTime = time;
+                    text.SetText($"{time:h:mm:ss} <size=+1><b>{time:tt}</b>");
+                }
+            }
+        }
+    }
+}
